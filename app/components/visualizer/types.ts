efc2b6b1af6801043d8b8d5b8ed2c350071174eb
xps-admin <strategicmindsadvisory@gmail.com>
2026-06-17 04:58:08 -0400

    Extend visualizer control types

--- app/components/visualizer/types.ts
+++ app/components/visualizer/types.ts
@@ -12,12 +12,25 @@ export type UploadedPreview = {
   type: string;
 };
 
+export type FlakeSize = "small" | "medium" | "large";
+export type MetallicPattern = "marble" | "waves" | "cloud";
+
 export type VisualizerControlsState = {
   opacity: number;
   gloss: number;
   textureScale: number;
   textureAngle: number;
   glitterSize: "fine" | "chunky";
+  flakeSize: FlakeSize;
+  flakeCoverage: number;
+  flakeLightPercent: number;
+  flakeMidPercent: number;
+  flakeDarkPercent: number;
+  mixerColorOne: string;
+  mixerColorTwo: string;
+  mixerColorThree: string;
+  metallicPattern: MetallicPattern;
+  metallicIntensity: number;
 };
 
 export type QuotePreviewPayload = {
