32486d679bd1f4152aa68f9577e387f8abd7c3aa
xps-admin <strategicmindsadvisory@gmail.com>
2026-06-17 00:34:32 -0400

    Use standardized color chart artwork and labeled popups

--- app/components/FinishVisualizer.tsx
+++ app/components/FinishVisualizer.tsx
@@ -34,108 +34,78 @@ type SelectedSample = {
 
 type FinishStyle = CSSProperties & Record<string, string>;
 
-const flakeChartImage = "https://cdn.shopify.com/s/files/1/0754/8905/0678/files/xps-top-12-epoxy-flake-color-chart.png?v=1781666861";
-const metallicChartImage = "https://cdn.shopify.com/s/files/1/0754/8905/0678/files/xps-xtreme-metallic-color-chart.webp?v=1781668409";
-const quartzChartImage = "https://cdn.shopify.com/s/files/1/0754/8905/0678/files/xps-quartz-sand-epoxy-color-chart.webp?v=1781668402";
+const flakeChartImage = "https://cdn.shopify.com/s/files/1/0754/8905/0678/files/xps-top-flake-colors-approved.png?v=1781670774";
+const metallicChartImage = "https://cdn.shopify.com/s/files/1/0754/8905/0678/files/xps-top-metallic-colors-standardized.png?v=1781670766";
+const quartzChartImage = "https://cdn.shopify.com/s/files/1/0754/8905/0678/files/xps-top-quartz-colors-standardized.png?v=1781670783";
 
-const flakeAreas = {
-  col1: "6.6%",
-  col2: "38.3%",
-  col3: "70.0%",
-  row1: "16.9%",
-  row2: "36.3%",
-  row3: "55.9%",
-  row4: "75.3%",
-  width: "25.7%",
-  height: "11.4%"
+const chartAreas = {
+  col1: "6.64%",
+  col2: "38.35%",
+  col3: "70.05%",
+  row1: "16.94%",
+  row2: "36.38%",
+  row3: "55.81%",
+  row4: "75.24%",
+  width: "25.65%",
+  height: "11.23%"
 };
 
-const metallicAreas = {
-  col1: "12.55%",
-  col2: "27.95%",
-  col3: "43.45%",
-  col4: "58.90%",
-  col5: "74.25%",
-  row1: "15.10%",
-  row2: "28.42%",
-  row3: "41.82%",
-  row4: "64.48%",
-  row5: "77.94%",
-  width: "14.50%",
-  height: "11.15%"
-};
-
-const quartzAreas = {
-  col1: "10.10%",
-  col2: "38.00%",
-  col3: "66.00%",
-  row1: "25.05%",
-  row2: "48.15%",
-  row3: "71.50%",
-  width: "23.95%",
-  height: "18.45%"
-};
-
-function area(left: string, top: string, width: string, height: string): ChartArea {
+function area(left: string, top: string, width = chartAreas.width, height = chartAreas.height): ChartArea {
   return { left, top, width, height };
 }
 
+function gridArea(index: number): ChartArea {
+  const columns = [chartAreas.col1, chartAreas.col2, chartAreas.col3];
+  const rows = [chartAreas.row1, chartAreas.row2, chartAreas.row3, chartAreas.row4];
+  return area(columns[index % 3], rows[Math.floor(index / 3)]);
+}
+
 const flakeOptions: ChartOption[] = [
-  { name: "Gravel", code: "FB-414", palette: ["#23303a", "#6f7f87", "#c9d0d2", "#f3f1e8"], area: area(flakeAreas.col1, flakeAreas.row1, flakeAreas.width, flakeAreas.height) },
-  { name: "Outback", code: "FB-517", palette: ["#2d2118", "#7d583a", "#c99e68", "#efe2c7"], area: area(flakeAreas.col2, flakeAreas.row1, flakeAreas.width, flakeAreas.height) },
-  { name: "Quicksilver", code: "FB-424", palette: ["#111a1c", "#566164", "#a8b2b1", "#f0f2ef"], area: area(flakeAreas.col3, flakeAreas.row1, flakeAreas.width, flakeAreas.height) },
-  { name: "Safari", code: "FB-504", palette: ["#292517", "#776543", "#c7b27b", "#f3e7c2"], area: area(flakeAreas.col1, flakeAreas.row2, flakeAreas.width, flakeAreas.height) },
-  { name: "Wombat", code: "FB-616", palette: ["#1d1f1f", "#6d6a60", "#aaa596", "#e4ddcf"], area: area(flakeAreas.col2, flakeAreas.row2, flakeAreas.width, flakeAreas.height) },
-  { name: "Stonehenge", code: "FB-427", palette: ["#14191d", "#7b898c", "#ccd7d7", "#f2f5ed"], area: area(flakeAreas.col3, flakeAreas.row2, flakeAreas.width, flakeAreas.height) },
-  { name: "Rapids", code: "FB-506", palette: ["#173343", "#4b8aa0", "#bdc7bd", "#d8caa7"], area: area(flakeAreas.col1, flakeAreas.row3, flakeAreas.width, flakeAreas.height) },
-  { name: "Creekbed", code: "FB-716", palette: ["#272016", "#756241", "#bda16a", "#e7d4ad"], area: area(flakeAreas.col2, flakeAreas.row3, flakeAreas.width, flakeAreas.height) },
-  { name: "Domino", code: "FB-411", palette: ["#050505", "#ffffff", "#8f989f", "#d8dde1"], area: area(flakeAreas.col3, flakeAreas.row3, flakeAreas.width, flakeAreas.height) },
-  { name: "Shoreline", code: "FB-421", palette: ["#172a34", "#738990", "#c4c8b9", "#e8ddc2"], area: area(flakeAreas.col1, flakeAreas.row4, flakeAreas.width, flakeAreas.height) },
-  { name: "Orbit", code: "FB-310", palette: ["#111821", "#5d6e77", "#bdc8d2", "#eef3f6"], area: area(flakeAreas.col2, flakeAreas.row4, flakeAreas.width, flakeAreas.height) },
-  { name: "Snowfall", code: "FB-602", palette: ["#4b5754", "#87938f", "#c9d1cc", "#f5f7f1"], area: area(flakeAreas.col3, flakeAreas.row4, flakeAreas.width, flakeAreas.height) }
+  { name: "Gravel", code: "FB-414", palette: ["#23303a", "#6f7f87", "#c9d0d2", "#f3f1e8"], area: gridArea(0) },
+  { name: "Outback", code: "FB-517", palette: ["#2d2118", "#7d583a", "#c99e68", "#efe2c7"], area: gridArea(1) },
+  { name: "Quicksilver", code: "FB-424", palette: ["#111a1c", "#566164", "#a8b2b1", "#f0f2ef"], area: gridArea(2) },
+  { name: "Safari", code: "FB-504", palette: ["#292517", "#776543", "#c7b27b", "#f3e7c2"], area: gridArea(3) },
+  { name: "Wombat", code: "FB-616", palette: ["#1d1f1f", "#6d6a60", "#aaa596", "#e4ddcf"], area: gridArea(4) },
+  { name: "Stonehenge", code: "FB-427", palette: ["#14191d", "#7b898c", "#ccd7d7", "#f2f5ed"], area: gridArea(5) },
+  { name: "Rapids", code: "FB-506", palette: ["#173343", "#4b8aa0", "#bdc7bd", "#d8caa7"], area: gridArea(6) },
+  { name: "Creekbed", code: "FB-716", palette: ["#272016", "#756241", "#bda16a", "#e7d4ad"], area: gridArea(7) },
+  { name: "Domino", code: "FB-411", palette: ["#050505", "#ffffff", "#8f989f", "#d8dde1"], area: gridArea(8) },
+  { name: "Shoreline", code: "FB-421", palette: ["#172a34", "#738990", "#c4c8b9", "#e8ddc2"], area: gridArea(9) },
+  { name: "Orbit", code: "FB-310", palette: ["#111821", "#5d6e77", "#bdc8d2", "#eef3f6"], area: gridArea(10) },
+  { name: "Snowfall", code: "FB-602", palette: ["#4b5754", "#87938f", "#c9d1cc", "#f5f7f1"], area: gridArea(11) }
 ];
 
 const metallicOptions: ChartOption[] = [
-  { name: "Arizona Gold", palette: ["#6d4510", "#b97818", "#dfad31", "#f2d167"], area: area(metallicAreas.col1, metallicAreas.row1, metallicAreas.width, metallicAreas.height) },
-  { name: "Autumn Blaze", palette: ["#5a2117", "#9e432c", "#c66e41", "#e19a64"], area: area(metallicAreas.col2, metallicAreas.row1, metallicAreas.width, metallicAreas.height) },
-  { name: "Cappuccino", palette: ["#2a1d13", "#5b3b20", "#8a6537", "#b48c54"], area: area(metallicAreas.col3, metallicAreas.row1, metallicAreas.width, metallicAreas.height) },
-  { name: "Caribbean Blue", palette: ["#142b64", "#214fb0", "#426ccd", "#7894d8"], area: area(metallicAreas.col4, metallicAreas.row1, metallicAreas.width, metallicAreas.height) },
-  { name: "Chestnut", palette: ["#2b130d", "#5b2717", "#7f3b20", "#a35b36"], area: area(metallicAreas.col5, metallicAreas.row1, metallicAreas.width, metallicAreas.height) },
-  { name: "Copper", palette: ["#5b331a", "#946234", "#b6864e", "#d4b07c"], area: area(metallicAreas.col1, metallicAreas.row2, metallicAreas.width, metallicAreas.height) },
-  { name: "Galaxy Blue", palette: ["#24444b", "#477c83", "#76a7ad", "#a7ced1"], area: area(metallicAreas.col2, metallicAreas.row2, metallicAreas.width, metallicAreas.height) },
-  { name: "Merlot", palette: ["#4a0f28", "#8a1742", "#bd285b", "#de5e85"], area: area(metallicAreas.col3, metallicAreas.row2, metallicAreas.width, metallicAreas.height) },
-  { name: "Purple Haze", palette: ["#443144", "#73556d", "#9a788f", "#c7abbc"], area: area(metallicAreas.col4, metallicAreas.row2, metallicAreas.width, metallicAreas.height) },
-  { name: "Quick Silver", palette: ["#4b4f4f", "#8b9290", "#c2c6c3", "#f1f1ef"], area: area(metallicAreas.col5, metallicAreas.row2, metallicAreas.width, metallicAreas.height) },
-  { name: "Sterling", palette: ["#6e7473", "#9fa4a3", "#cfd2d1", "#f5f5f3"], area: area(metallicAreas.col1, metallicAreas.row3, metallicAreas.width, metallicAreas.height) },
-  { name: "Tuscan Sun", palette: ["#714019", "#a8662c", "#d49552", "#efc27e"], area: area(metallicAreas.col2, metallicAreas.row3, metallicAreas.width, metallicAreas.height) },
-  { name: "Blurple", palette: ["#1e2d4f", "#425481", "#69759a", "#9da5bb"], area: area(metallicAreas.col1, metallicAreas.row4, metallicAreas.width, metallicAreas.height) },
-  { name: "Burlywood", palette: ["#3f291a", "#735131", "#9a7a50", "#c9ae82"], area: area(metallicAreas.col2, metallicAreas.row4, metallicAreas.width, metallicAreas.height) },
-  { name: "Celestial Blue", palette: ["#183040", "#315a6f", "#5e8190", "#99b5bd"], area: area(metallicAreas.col3, metallicAreas.row4, metallicAreas.width, metallicAreas.height) },
-  { name: "Cerulean Blue", palette: ["#1d5c64", "#43858d", "#72aeb3", "#aad6d9"], area: area(metallicAreas.col4, metallicAreas.row4, metallicAreas.width, metallicAreas.height) },
-  { name: "Cinnabar", palette: ["#4b160b", "#8c321a", "#b14b29", "#d77951"], area: area(metallicAreas.col5, metallicAreas.row4, metallicAreas.width, metallicAreas.height) },
-  { name: "Mango Tango", palette: ["#5b2a0d", "#9b551b", "#c8812a", "#e7b456"], area: area(metallicAreas.col1, metallicAreas.row5, metallicAreas.width, metallicAreas.height) },
-  { name: "Mayan Gold", palette: ["#7a4a16", "#aa7627", "#d3a441", "#f0ce79"], area: area(metallicAreas.col2, metallicAreas.row5, metallicAreas.width, metallicAreas.height) },
-  { name: "Onyx", palette: ["#050505", "#1a1f1f", "#444b4a", "#7e8584"], area: area(metallicAreas.col3, metallicAreas.row5, metallicAreas.width, metallicAreas.height) },
-  { name: "Perfect Storm", palette: ["#27302c", "#4c645b", "#759081", "#b7c7b7"], area: area(metallicAreas.col4, metallicAreas.row5, metallicAreas.width, metallicAreas.height) },
-  { name: "Sequoia", palette: ["#321916", "#5d322c", "#80514b", "#a87870"], area: area(metallicAreas.col5, metallicAreas.row5, metallicAreas.width, metallicAreas.height) }
+  { name: "Arizona Gold", palette: ["#6d4510", "#b97818", "#dfad31", "#f2d167"], area: gridArea(0) },
+  { name: "Autumn Blaze", palette: ["#5a2117", "#9e432c", "#c66e41", "#e19a64"], area: gridArea(1) },
+  { name: "Cappuccino", palette: ["#2a1d13", "#5b3b20", "#8a6537", "#b48c54"], area: gridArea(2) },
+  { name: "Caribbean Blue", palette: ["#142b64", "#214fb0", "#426ccd", "#7894d8"], area: gridArea(3) },
+  { name: "Chestnut", palette: ["#2b130d", "#5b2717", "#7f3b20", "#a35b36"], area: gridArea(4) },
+  { name: "Copper", palette: ["#5b331a", "#946234", "#b6864e", "#d4b07c"], area: gridArea(5) },
+  { name: "Galaxy Blue", palette: ["#24444b", "#477c83", "#76a7ad", "#a7ced1"], area: gridArea(6) },
+  { name: "Merlot", palette: ["#4a0f28", "#8a1742", "#bd285b", "#de5e85"], area: gridArea(7) },
+  { name: "Purple Haze", palette: ["#443144", "#73556d", "#9a788f", "#c7abbc"], area: gridArea(8) },
+  { name: "Quick Silver", palette: ["#4b4f4f", "#8b9290", "#c2c6c3", "#f1f1ef"], area: gridArea(9) },
+  { name: "Sterling", palette: ["#6e7473", "#9fa4a3", "#cfd2d1", "#f5f5f3"], area: gridArea(10) },
+  { name: "Tuscan Sun", palette: ["#714019", "#a8662c", "#d49552", "#efc27e"], area: gridArea(11) }
 ];
 
 const quartzOptions: ChartOption[] = [
-  { name: "Limestone Quartz", palette: ["#b7bdbd", "#d5dada", "#f1f2ef", "#8e999a"], area: area(quartzAreas.col1, quartzAreas.row1, quartzAreas.width, quartzAreas.height) },
-  { name: "Slate Quartz", palette: ["#9aa7ac", "#c6d2d7", "#e8ecea", "#647278"], area: area(quartzAreas.col2, quartzAreas.row1, quartzAreas.width, quartzAreas.height) },
-  { name: "Flint Quartz", palette: ["#81898d", "#b3babf", "#d7dadd", "#4d585d"], area: area(quartzAreas.col3, quartzAreas.row1, quartzAreas.width, quartzAreas.height) },
-  { name: "Evening Quartz", palette: ["#5e6868", "#8a9491", "#c1c6bd", "#e7e5d6"], area: area(quartzAreas.col1, quartzAreas.row2, quartzAreas.width, quartzAreas.height) },
-  { name: "Cobblestone Quartz", palette: ["#aaa3a0", "#d5cfca", "#eee9df", "#85807c"], area: area(quartzAreas.col2, quartzAreas.row2, quartzAreas.width, quartzAreas.height) },
-  { name: "Mojave Quartz", palette: ["#b6a994", "#d7cbb8", "#eee1cb", "#8d806c"], area: area(quartzAreas.col3, quartzAreas.row2, quartzAreas.width, quartzAreas.height) },
-  { name: "Sandstone Quartz", palette: ["#9f917c", "#c7b9a2", "#e8ddc8", "#756854"], area: area(quartzAreas.col1, quartzAreas.row3, quartzAreas.width, quartzAreas.height) },
-  { name: "Blue Moon Quartz", palette: ["#7ca4b3", "#a9c7d3", "#d4e3e7", "#557584"], area: area(quartzAreas.col2, quartzAreas.row3, quartzAreas.width, quartzAreas.height) },
-  { name: "Brick Quartz", palette: ["#8b4d42", "#b87868", "#d6a293", "#64322c"], area: area(quartzAreas.col3, quartzAreas.row3, quartzAreas.width, quartzAreas.height) }
+  { name: "Limestone", code: "Quartz", palette: ["#b7bdbd", "#d5dada", "#f1f2ef", "#8e999a"], area: gridArea(0) },
+  { name: "Slate", code: "Quartz", palette: ["#9aa7ac", "#c6d2d7", "#e8ecea", "#647278"], area: gridArea(1) },
+  { name: "Flint", code: "Quartz", palette: ["#81898d", "#b3babf", "#d7dadd", "#4d585d"], area: gridArea(2) },
+  { name: "Evening", code: "Quartz", palette: ["#5e6868", "#8a9491", "#c1c6bd", "#e7e5d6"], area: gridArea(3) },
+  { name: "Cobblestone", code: "Quartz", palette: ["#aaa3a0", "#d5cfca", "#eee9df", "#85807c"], area: gridArea(4) },
+  { name: "Mojave", code: "Quartz", palette: ["#b6a994", "#d7cbb8", "#eee1cb", "#8d806c"], area: gridArea(5) },
+  { name: "Sandstone", code: "Quartz", palette: ["#9f917c", "#c7b9a2", "#e8ddc8", "#756854"], area: gridArea(6) },
+  { name: "Blue Moon", code: "Quartz", palette: ["#7ca4b3", "#a9c7d3", "#d4e3e7", "#557584"], area: gridArea(7) },
+  { name: "Brick", code: "Quartz", palette: ["#8b4d42", "#b87868", "#d6a293", "#64322c"], area: gridArea(8) }
 ];
 
 const chartBoards: ChartBoard[] = [
   { id: "flake", width: 1536, height: 2048, image: flakeChartImage, alt: "XPS top 12 epoxy flake color chart", options: flakeOptions },
-  { id: "metallic", width: 1275, height: 1650, image: metallicChartImage, alt: "XPS Xtreme metallic color chart", options: metallicOptions },
-  { id: "quartz", width: 1583, height: 2048, image: quartzChartImage, alt: "XPS quartz sand system color chart", options: quartzOptions }
+  { id: "metallic", width: 1536, height: 2048, image: metallicChartImage, alt: "XPS top metallic colors chart", options: metallicOptions },
+  { id: "quartz", width: 1536, height: 2048, image: quartzChartImage, alt: "XPS top quartz colors chart", options: quartzOptions }
 ];
 
 function finishStyle(option: ChartOption): FinishStyle {
@@ -260,6 +230,7 @@ export function FinishVisualizer() {
                           key={`${board.id}-${option.name}-popup`}
                         >
                           <img src={board.image} alt="" style={popupImageStyle(option)} aria-hidden="true" />
+                          <span className="xps-color-popup-label">{option.name}</span>
                         </div>
                       ) : null
                     ))}
@@ -268,6 +239,9 @@ export function FinishVisualizer() {
               );
             })}
           </div>
+          <p className="xps-chart-disclaimer">
+            Due to computer screen differences, some colors may slightly differ in person, and especially when sealer is applied which may give it a &quot;wet look&quot; which enrichens the color.
+          </p>
         </div>
       </section>
     </>
